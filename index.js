5b6e96d6bea74e52ea083d3a3966935ffe747737
Nazeem <nazeem.s2k@gmail.com>
2021-08-22 19:41:51 +0500

    3.18

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const Person = require('./models/person')
+const { response } = require('express')
 
 morgan.token('body', req => JSON.stringify(req.body))
 
@@ -16,15 +17,31 @@ app.use(morgan('tiny'))
 app.use(morgan(':body'))
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(person => {
-    res.json(person)
+  Person.find({}).then(persons => {
+    res.json(persons)
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person)
+app.get('/info', (req, res) => {
+  Person.find({}).then(persons => {
+    const date = new Date()
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+         <p>${date}</p>`
+    )
   })
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+  .then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => next(error))  
 })
 
 app.post('/api/persons', (req, res) => {
@@ -44,8 +61,48 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
